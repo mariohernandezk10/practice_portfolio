1f686106fdeba53abd5f9ca8b2db5c9827323f87
Mario Hernandez <mariohernandezk10@gmail.com>
2020-12-05 15:45:26 -0600

    added picture and deleted comments in js

--- script.js
+++ script.js
@@ -27,11 +27,3 @@ $(document).ready(function () {
         once: true
     });
 })
-
-// const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
-
-// tl.to('.text', {y: "0%", duration: 1});
-// tl.to(".slider", {y: "-100%", duration: 2, delay: 0});
-// tl.to(".intro", {y: "-100%", duration: 1});
-// tl.fromTo("header", {opacity: 0}, {opacity: 1, duration: 1});
-// tl.fromTo("body", {opacity: 0}, {opacity: 1, duration: 1});
